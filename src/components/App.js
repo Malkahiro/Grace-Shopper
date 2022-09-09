8efcc8da111ffda7842afc2f66d88a0b5694f94b
Jacob Joseph Jorishie <jake.jorishie@gmail.com>
2022-09-08 20:35:58 -0500

    Non-Functioning Login and Register Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+// import { Routes, Route } from "react-router-dom";
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
 import { getAPIHealth } from '../axios-services';
 import '../style/App.css';
+import Login from './Login/Login'
+import Register from './Register/Register';
 
 const App = () => {
   const [APIHealth, setAPIHealth] = useState('');
@@ -23,7 +26,9 @@ const App = () => {
   }, []);
 
   return (
+    
     <div className="app-container">
+      <Register />
       <h1>Hello, World!</h1>
       <p>API Status: {APIHealth}</p>
     </div>
