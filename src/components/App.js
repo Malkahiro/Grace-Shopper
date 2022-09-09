8293a7bf22043e9a53acd02170dea3940649d192
Jacob Joseph Jorishie <jake.jorishie@gmail.com>
2022-09-08 20:59:04 -0500

    non functioning login and reg with routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 // getAPIHealth is defined in our axios-services directory index.js
 // you can think of that directory as a collection of api adapters
 // where each adapter fetches specific info from our express server's /api route
@@ -28,9 +28,16 @@ const App = () => {
   return (
     
     <div className="app-container">
-      <Register />
-      <h1>Hello, World!</h1>
-      <p>API Status: {APIHealth}</p>
+      <Routes>
+       <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+        </Routes>
     </div>
   );
 };
