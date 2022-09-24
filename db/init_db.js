b5c9bf25f9af1c3205eb689adc027d5ad4665117
Chris <eyolo87@gmail.com>
2022-09-24 09:04:19 -0500

    added unique requirement for email

--- db/init_db.js
+++ db/init_db.js
@@ -31,7 +31,7 @@ async function buildTables() {
         username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
         address VARCHAR(255) NOT NULL,
         "isAdmin" BOOLEAN DEFAULT false
         
